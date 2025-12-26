86d52758a6bf7d66fe179570a204cef1c9cedf5b
chisaelim <chisae0123@gmail.com>
2025-12-26 10:11:44 +0700

    edit index page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     include './includes/header.inc.php';
     include './includes/navbar.inc.php';
 ?>
-    <h1>Index</h1>
+    <h1>Index Home</h1>
 <?php
     include './includes/footer.inc.php'
 ?>
